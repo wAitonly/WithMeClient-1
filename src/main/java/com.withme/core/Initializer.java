3aea0fab4afcf06a3ca02de9671670ccc39d1c34
zhaobenquan <393614249@qq.com>
2019-11-18 09:36:43 +0800

    中文编码

--- src/main/java/com.withme/core/Initializer.java
+++ src/main/java/com.withme/core/Initializer.java
@@ -6,6 +6,8 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 
+import java.nio.charset.Charset;
+
 /**
  * 管道初始化
  * @author zhaobbenquan 2019/10/15
@@ -14,8 +16,8 @@ public class Initializer extends ChannelInitializer<NioSocketChannel> {
     @Override
     protected void initChannel(NioSocketChannel nioSocketChannel){
         nioSocketChannel.pipeline()
-                .addLast(new StringEncoder())
-                .addLast(new StringDecoder())
+                .addLast(new StringEncoder(Charset.forName("UTF-8")))
+                .addLast(new StringDecoder(Charset.forName("UTF-8")))
                 .addLast(new MessageHandler());
     }
 }
